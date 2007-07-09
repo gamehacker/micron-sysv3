244ec49677f0d17c01ea6433149795e9f5adcaf2
martintang25 <martintang25@64417f38-0733-0410-b9bd-471ee5dae657>
2007-07-09 13:21:09 +0000

    reupload!

--- trunk/kernel/arch/i386/init.c
+++
@@ -1,9 +0,0 @@
-/*****************************************************************************
- * Micron System V3 - I386 Boot & Loader
- * Copyright (C) 2007, Huaxin Tang
- * PROTECTED UNDER MICRON SYSTEM PUBLIC LICENSE AGREEMENT.
- *****************************************************************************/
-void _start()
-{
-}
-
