0fdb3d751d29895691e0f4b328ef3fea716afe0e
martintang25 <martintang25@64417f38-0733-0410-b9bd-471ee5dae657>
2007-07-13 16:51:43 +0000

    added up some fixes

---
+++ sysv3/kernel/arch/i386/idt.c
@@ -0,0 +1,7 @@
+/*****************************************************************************
+ * Micron System V3 - I386 Architecture Driver
+ * Copyright (C) 2007, Micron System Team
+ * Copyright (C) 2007, Martin Tang
+ * PROTECTED UNDER MICRON SYSTEM PUBLIC LICENSE AGREEMENT.
+ *****************************************************************************/
+
